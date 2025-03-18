f49e7cdab8ad65c10d76ce12a3cdf0c6fe604bc8
Joel Munoz <j.munoz1@yahoo.com>
2025-03-17 22:47:11 -0700

    project branch created

---
+++ src/Components/ProjectItem/ProjectItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./projectItem.scss"
+
+function ProjectItem(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ProjectItem;
